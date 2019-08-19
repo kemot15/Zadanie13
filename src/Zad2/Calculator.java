c486e6754d141959dee5b4b522bb378b6b1bbdf5
Tomek <kemot15@tlen.pl>
2019-08-19 15:28:44 +0200

    zad 2 ver 2

--- src/Zad2/Calculator.java
+++ src/Zad2/Calculator.java
@@ -5,54 +5,45 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        calc("D:\\Calc.txt", "D:\\CalcResult.txt");
+        try {
+            calc("G:\\Calc.txt", "G:\\CalcResult.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void calc (String pathRead, String pathWrite){
+    private static void calc (String pathRead, String pathWrite) throws IOException {
         File fileReader = new File(pathRead);
-
-        File fileWriter = new File(pathWrite);
-        try {
             Scanner input = new Scanner(fileReader);
             FileWriter fw = new FileWriter(pathWrite);
             BufferedWriter bw = new BufferedWriter(fw);
             while (input.hasNextLine()){
                 String[] line = input.nextLine().split(" ");
-                String lineToWrite;
+                String lineToWrite = null;
                 int a = Integer.parseInt(line[0]);
                 int b = Integer.parseInt(line[2]);
-                int result = 0;
-                switch (line[1]){
-                    case "+":
-                        result = a + b;
-                        lineToWrite = a + " + " + b + " = " + result +"\n";
-                        System.out.print(lineToWrite);
-                        bw.write(lineToWrite);
-                        break;
-                    case "-":
-                        result = a - b;
-                        lineToWrite = a + " - " + b + " = " + result + "\n";
-                        System.out.print(lineToWrite);
-                        bw.write(lineToWrite);
-                        break;
-                    case "*":
-                        result = a * b;
-                        lineToWrite = a + " * " + b + " = " + result +"\n";
-                        System.out.print(lineToWrite);
-                        bw.write(lineToWrite);
-                        break;
-                    case "/":
-                        result = a / b;
-                        lineToWrite = a + " / " + b + " = " + result +"\n";
-                        System.out.print(lineToWrite);
-                        bw.write(lineToWrite);
-                        break;
-                }
+                lineToWrite = a + " " + line[1] + " " + b + " = " + getResult(line[1], a, b) +"\n";
+                System.out.print(lineToWrite);
+                bw.write(lineToWrite);
             }
             input.close();
             bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+
+    private static int getResult (String sign, int a, int b){
+        switch (sign){
+            case "+":
+                return a+b;
+            case "-":
+                return a-b;
+            case "*":
+                return a*b;
+            case "/":
+                return a/b;
+                default:
+                    return 0;
         }
-}
+
+    }
+
 }
